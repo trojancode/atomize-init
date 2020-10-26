85b39abfc2b21366afc29e79aeecb96d05c5f8aa
TROJANCODE <codeboycodes@gmail.com>
2020-10-26 15:22:00 +0530

    Attomixe working

--- src/Routes/Home2.js
+++ src/Routes/Home2.js
@@ -1,12 +1,33 @@
 import React, { useState } from 'react'
-import { Div, Icon, Button } from "atomize";
+import { Div, Icon, Button, Anchor,Dropdown } from "atomize";
 import { Link } from 'react-router-dom';
 
 const Home2 = () => {
     const [showDropdown, setShowDropdown] = useState(false)
+
+
+    const menuList = (
+        <Div>
+            {["Option 1", "Option 2", "Option 3"].map((name, index) => (
+                <Anchor d="block" p={{ y: "0.25rem" }}>
+                    {name}
+                </Anchor>
+            ))}
+        </Div>
+    );
+
     return (
         <Div>
-        <Button>asdsad 22222</Button>
+            <Dropdown
+                w="fit-content"
+                isOpen={showDropdown}
+                onClick={() =>
+                    setShowDropdown(!showDropdown )
+                }
+                menu={menuList}
+            >
+                Click me
+      </Dropdown>
         <Link to='/'>
         eee22
         </Link>
